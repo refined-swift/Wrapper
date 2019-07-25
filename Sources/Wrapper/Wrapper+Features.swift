dc7378daa8de0bfa7d764fec9cfc934a51dfdc49
hectr <h@mrhector.me>
2019-07-26 01:30:59 +0200

    Wrap letter, lowercase, number, uppercase and whitespace features

--- Sources/Wrapper/Wrapper+Features.swift
+++ Sources/Wrapper/Wrapper+Features.swift
@@ -13,3 +13,33 @@ extension Wrapper where WrappedValue: MaybeEmpty {
         return wrapped.isEmpty
     }
 }
+
+extension Wrapper where WrappedValue: MaybeLetter {
+    public var isLetter: Bool {
+        return wrapped.isLetter
+    }
+}
+
+extension Wrapper where WrappedValue: MaybeLowercase {
+    public var isLowercase: Bool {
+        return wrapped.isLowercase
+    }
+}
+
+extension Wrapper where WrappedValue: MaybeNumber {
+    public var isNumber: Bool {
+        return wrapped.isNumber
+    }
+}
+
+extension Wrapper where WrappedValue: MaybeUppercase {
+    public var isUppercase: Bool {
+        return wrapped.isUppercase
+    }
+}
+
+extension Wrapper where WrappedValue: MaybeWhitespace {
+    public var isWhitespace: Bool {
+        return wrapped.isWhitespace
+    }
+}
